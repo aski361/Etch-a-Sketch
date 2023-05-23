84b312d1ab623ac8a2f45305d265dd093e5e7b2a
aski361 <karpowiczaskaniusz@outlook.com>
2023-05-23 23:22:30 +0200

    added random colors and boxes are colored now when mouse is on div

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ for(i=0;i<(n*n);i++) {
     div.style.width = `${600/n}px`
     div.style.height = `${600/n}px`
     div.classList.add('box');
+    div.setAttribute('onmouseenter', 'color(this)');
     container.appendChild(div);
 }
 
+function mathRandom() {
+    return Math.ceil(Math.random() * 255)
+} 
+
+
+
+function color(x) {
+    x.style.background = `rgb(${mathRandom()},${mathRandom()},${mathRandom()}`;
+  }
