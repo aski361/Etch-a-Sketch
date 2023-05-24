976fb6e48236b1b786dfbbc0f1c48e49cee031d0
aski361 <karpowiczaskaniusz@outlook.com>
2023-05-24 09:42:25 +0200

    finished project

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const container = document.querySelector('.container');
 
-const n = 16;
+let n = 16;
 
-for(i=0;i<(n*n);i++) {
-    const div = document.createElement('div');
-    div.style.border = '1px solid black'
-    div.style.width = `${600/n}px`
-    div.style.height = `${600/n}px`
-    div.classList.add('box');
-    div.setAttribute('onmouseenter', 'color(this)');
-    container.appendChild(div);
+function createSquares() {
+    for(i=0;i<(n*n);i++) {
+        const div = document.createElement('div');
+        div.style.border = '1px solid black'
+        div.style.width = `${600/n}px`
+        div.style.height = `${600/n}px`
+        div.classList.add('box');
+        div.setAttribute('onmouseenter', 'color(this)');
+        container.appendChild(div);
+    }
 }
+
+createSquares();
 
 function mathRandom() {
     return Math.ceil(Math.random() * 255)
 } 
 
-
-
 function color(x) {
     x.style.background = `rgb(${mathRandom()},${mathRandom()},${mathRandom()}`;
-  }
+}
+
+const btn = document.querySelector('.how');
+
+function button() {
+    const box = document.querySelectorAll('.box');
+    box.forEach(item => container.removeChild(item));
+    n = prompt('How many squares per side do you want?');
+    n = Number(n);
+    createSquares();
+}
+
+
+btn.addEventListener('click', button);
